b8449bde5cbc9a64621e15807d9f4afd6f6da2d3
AKTM <andrea.yk44@guru.smp.belajar.id>
2026-09-11 10:41:32 +0700

    Refactor CONFIG object in config.js

--- js/config.js
+++ js/config.js
@@ -1,9 +1,19 @@
 const CONFIG = {
     SPREADSHEET_API_URL: "https://script.google.com/macros/s/AKfycbxF3g7LSwr61mNuVqOApDXnmjZsY5putWlumzL_GnJJN8hOPqhTk0MR9aPRnmbFJX1x/exec",
+    SUBMIT_API_URL: "https://script.google.com/macros/s/AKfycbym_6liYh9AXBoHuDr_kuvuJzyxb1JNfYQezHGTlUOQVmJxU23qzkhVLOt7o3WCT1tM/exec",
+    DURASI_MAPEL: {
+        "PKN": 90,
+        "MTK": 120,
+        "IPA": 90,
+        "IPS": 90,
+        "B_INDO": 120,
+        "B_INGG": 120,
+        "PAI": 90
+    },
     PROCTOR: {
-        ENABLE_ANTI_TAB_SWITCH: true,      // Deteksi keluar halaman / pindah tab
-        MAX_TAB_SWITCH_WARNINGS: 3,        // Batas maksimal peringatan pindah tab
-        ENABLE_DISABLE_RIGHT_CLICK: true,  // Matikan klik kanan
-        ENABLE_DISABLE_DEVTOOLS_KEYS: true // Matikan tombol F12, Ctrl+Shift+I, dll
+        ENABLE_ANTI_TAB_SWITCH: true,
+        MAX_TAB_SWITCH_WARNINGS: 3,
+        ENABLE_DISABLE_RIGHT_CLICK: true,
+        ENABLE_DISABLE_DEVTOOLS_KEYS: true
     }
 };
