6ac52bb72800948136e8c9059986fdd73a1ba229
AKTM <andrea.yk44@guru.smp.belajar.id>
2026-09-11 10:29:01 +0700

    Add configuration for proctoring settings

---
+++ js/config.js
@@ -0,0 +1,9 @@
+const CONFIG = {
+    SPREADSHEET_API_URL: "https://script.google.com/macros/s/AKfycbxF3g7LSwr61mNuVqOApDXnmjZsY5putWlumzL_GnJJN8hOPqhTk0MR9aPRnmbFJX1x/exec",
+    PROCTOR: {
+        ENABLE_ANTI_TAB_SWITCH: true,      // Deteksi keluar halaman / pindah tab
+        MAX_TAB_SWITCH_WARNINGS: 3,        // Batas maksimal peringatan pindah tab
+        ENABLE_DISABLE_RIGHT_CLICK: true,  // Matikan klik kanan
+        ENABLE_DISABLE_DEVTOOLS_KEYS: true // Matikan tombol F12, Ctrl+Shift+I, dll
+    }
+};
