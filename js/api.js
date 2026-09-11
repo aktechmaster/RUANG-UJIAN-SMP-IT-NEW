ce657d12a1278654def64463b95e1f85f2ea02b6
AKTM <andrea.yk44@guru.smp.belajar.id>
2026-09-11 10:41:47 +0700

    Add fetchSoal and submitJawaban methods in api.js

--- js/api.js
+++ js/api.js
@@ -15,5 +15,24 @@ const API = {
         const response = await fetch(`${CONFIG.SPREADSHEET_API_URL}?action=getToken&mapel=${encodeURIComponent(mapel)}`);
         if (!response.ok) throw new Error('Gagal memvalidasi token.');
         return await response.json();
+    },
+
+    // --- Khusus Ujian ---
+    async fetchSoal(tingkatKelas, mapelUjian) {
+        const namaFile = `soal_${tingkatKelas}_${mapelUjian}.json`;
+        const urlJSON = `./Soal_Kelas_${tingkatKelas}/${namaFile}?t=` + new Date().getTime();
+        const response = await fetch(urlJSON);
+        if (!response.ok) throw new Error(`Gagal memuat berkas ${namaFile}`);
+        return await response.json();
+    },
+
+    async submitJawaban(dataSiswa) {
+        const response = await fetch(CONFIG.SUBMIT_API_URL, {
+            method: 'POST',
+            headers: { 'Content-Type': 'text/plain;charset=utf-8' },
+            body: JSON.stringify(dataSiswa)
+        });
+        if (!response.ok) throw new Error("Respon server bermasalah (Status: " + response.status + ")");
+        return await response.json();
     }
 };
